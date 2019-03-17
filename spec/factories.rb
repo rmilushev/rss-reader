2359ca87cfaa0fc97d8cae3c55eaee42ff346504
Rumen Milushev <r@vert.bg>
2019-03-17 21:12:47 +0200

    Add model test

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+
+  factory :location do
+    # title 'RSS feed'
+    url 'https://cre.actor/blog/rss.xml'
+  end
+end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Location, type: :model do
-  pending
+# RSpec.describe Location, type: :model do
+#   pending
+# end
+#
+describe Location, 'validation' do
+  it { should validate_presence_of(:url) }
 end
